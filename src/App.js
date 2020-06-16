2a35051384f54060766bbbea03c1fbc691f43b1f
zwq245283 <zwq245283@alibaba-inc.com>
2020-06-16 14:47:35 +0800

    test

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
           <h1 className="App-title">hello</h1>
+          <h1 className="App-title">hello</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
